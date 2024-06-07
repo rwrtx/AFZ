6af882024df7bda42b9e5883056d240c13084e45
rwrtx <152061298+rwrtx@users.noreply.github.com>
2024-06-07 09:56:21 +0700

    Update cf.sh

--- Fls/cf.sh
+++ Fls/cf.sh
@@ -3,20 +3,30 @@ MYIP=$(wget -qO- icanhazip.com);
 apt install jq curl -y
 clear
 echo -e ""
-echo -e "\033[96;1m============================\033[0m"
-echo -e "\033[93;1m      INPUT SUBDOMAIN"
-echo -e "\033[96;1m============================\033[0m"
-echo -e "\033[91;1m Note. contoh Subdomain :\033[0m \033[93mbodoh22 \033[0m"
-echo -e " "
-read -p "SUBDOMAIN :  " domen
+echo -e "\e[94;1m╔═════════════════════════════════════════════════╗$NC"
+echo -e "\033[96;1m                 INPUT SUBDOMAIN \e[0m"
+echo -e "\e[94;1m╚═════════════════════════════════════════════════╝ $NC"
 echo -e ""
-DOMAIN=mypremi.my.id
+echo -e "\e[91;1m  --[ BENAR ]--\e[0m"
+echo -e ""
+echo -e "\e[93;1m  • \e[92;1m bodoh , tai , cadux \e[0m"
+echo -e ""
+echo -e "\e[92;1m  --[ SALAH ]--\e[0m"
+echo -e ""
+echo -e "\e[93;1m  • \e[91;1m bodoh.my.id , tai.com , cadux.cfd \e[0m"
+echo -e " "
+echo -e "\e[94;1m╚═════════════════════════════════════════════════╝ $NC"
+echo -e " "
+echo -e " "
+read -p "   INPUT SUBDOMAIN :  " domen
+echo -e ""
+DOMAIN=r32wrtxtunneling.site
 sub=${domen}
-dns=${sub}.mypremi.my.id
+dns=${sub}.r32wrtxtunneling.site
 #(</dev/urandom tr -dc a-z0-9 | head -c5)
-dns=${sub}.mypremi.my.id
-CF_KEY=ab87a9345d29e5fb48d447e422377ebf3d1fd
-CF_ID=sabdestore@gmail.com
+#dns=${sub}.r32wrtxtunneling.site
+CF_KEY=3c45413104a3c39f1db9dff20acb3a99d7610
+CF_ID=amandafitrizharifa009@gmail.com
 set -euo pipefail
 IP=$(wget -qO- icanhazip.com);
 echo "Updating DNS for ${dns}..."
